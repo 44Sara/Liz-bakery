a4f132426f5f2d83220dce5b4b4f60bd8643eaaf
Jerusalem Hailu <hailus02@gmail.com>
2025-07-21 13:25:38 -0400

    Update homepage and footer styling

--- js/jquery-sticky-menu.js
+++ js/jquery-sticky-menu.js
@@ -1,20 +1,10 @@
+
+
 // Sticky Header
-$(window).scroll(function() {
-
+$(window).scroll(function () {
     if ($(window).scrollTop() > 100) {
-        $('.main_h').addClass('sticky');
+        $('.sticky-header').addClass('sticky');
     } else {
-        $('.main_h').removeClass('sticky');
-    }
-});
-
-// if you want to add stick menu on custom div scroll
-
-$(window).scroll(function() {
-
-    if ($("your_div").scrollTop() > 100) {
-        $('.main_h').addClass('sticky');
-    } else {
-        $('.main_h').removeClass('sticky');
+        $('.sticky-header').removeClass('sticky');
     }
 });
